93834a96beef94f86d40801a8f282d88daed2a31
Yuxuan Xie <teststaybaka@gmail.com>
2024-08-06 16:28:42 -0700

    Try connect to postgres.

--- self/frontend/sign_in_handler.ts
+++ self/frontend/sign_in_handler.ts
@@ -1,5 +1,6 @@
 import AWS = require("aws-sdk");
-import getStream from "get-stream";
+import getStream = require("get-stream");
+import postgres = require("postgres");
 import {
   SignInRequestBody,
   SignInResponse,
@@ -8,13 +9,11 @@ import { SignInHandlerInterface } from "@phading/user_service_interface/self/fro
 import { ClientSession } from "@phading/user_session_service_interface/client_session";
 import { SessionBuilder } from "@selfage/service_handler/session_signer";
 
-AWS.config.update({ region: "us-east-1" });
-
 export class SignInHandler extends SignInHandlerInterface {
   public static create(): SignInHandler {
     return new SignInHandler(
       SessionBuilder.create(),
-      new AWS.S3({ apiVersion: "2006-03-01" }),
+      new AWS.S3({ apiVersion: "2006-03-01", region: "us-east-1" }),
     );
   }
 
@@ -38,6 +37,17 @@ export class SignInHandler extends SignInHandlerInterface {
         })
         .createReadStream(),
     );
+    const sql = postgres({
+      host: "user-service-db.cda5j8gofxqp.us-east-1.rds.amazonaws.com",
+      port: 5432,
+      database: "UserServiceDb",
+      username: "root",
+      password: dbPassword,
+    });
+    await sql`CREATE TABLE IF NOT EXISTS User (
+      username varchar(128)
+    );`;
+    await sql.end();
 
     let signedSession = this.sessionBuilder.build(
       JSON.stringify({
