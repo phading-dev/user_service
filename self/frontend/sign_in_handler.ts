403e9e9b1341dd6d4e782fc179ffbd4d4b99dca4
Yuxuan Xie <teststaybaka@gmail.com>
2024-08-06 11:44:26 -0700

    Try read password from S3.

--- self/frontend/sign_in_handler.ts
+++ self/frontend/sign_in_handler.ts
@@ -1,3 +1,5 @@
+import AWS = require("aws-sdk");
+import getStream from "get-stream";
 import {
   SignInRequestBody,
   SignInResponse,
@@ -6,12 +8,20 @@ import { SignInHandlerInterface } from "@phading/user_service_interface/self/fro
 import { ClientSession } from "@phading/user_session_service_interface/client_session";
 import { SessionBuilder } from "@selfage/service_handler/session_signer";
 
+AWS.config.update({ region: "us-east-1" });
+
 export class SignInHandler extends SignInHandlerInterface {
-  public static create(): SignInHandler {
-    return new SignInHandler(SessionBuilder.create());
+  public static create(s3Service: AWS.S3): SignInHandler {
+    return new SignInHandler(
+      SessionBuilder.create(),
+      new AWS.S3({ apiVersion: "2006-03-01" }),
+    );
   }
 
-  public constructor(private sessionBuilder: SessionBuilder) {
+  public constructor(
+    private sessionBuilder: SessionBuilder,
+    private s3Service: AWS.S3,
+  ) {
     super();
   }
 
@@ -20,9 +30,18 @@ export class SignInHandler extends SignInHandlerInterface {
     body: SignInRequestBody,
   ): Promise<SignInResponse> {
     console.log(`${loggingPrefix} Start handling SignIn request.`);
+    let dbPassword = await getStream(
+      this.s3Service
+        .getObject({
+          Bucket: "user-service-secrets",
+          Key: "db_password",
+        })
+        .createReadStream(),
+    );
+
     let signedSession = this.sessionBuilder.build(
       JSON.stringify({
-        sessionId: `${body.username}-${body.password}`,
+        sessionId: `${body.username}-${body.password}-${dbPassword}`,
       } as ClientSession),
     );
     return {
