124036ab7d755b97e5ebdeed310388360c6a1e2e
Yuxuan Xie <teststaybaka@gmail.com>
2024-08-12 14:42:58 -0700

    Try Prometheus custom metrics.

--- self/frontend/sign_in_handler.ts
+++ self/frontend/sign_in_handler.ts
@@ -1,3 +1,4 @@
+import promClient = require("prom-client");
 import {
   SignInRequestBody,
   SignInResponse,
@@ -5,6 +6,22 @@ import {
 import { SignInHandlerInterface } from "@phading/user_service_interface/self/frontend/server_handlers";
 import { ClientSession } from "@phading/user_session_service_interface/client_session";
 import { SessionBuilder } from "@selfage/service_handler/session_signer";
+
+// let METRICS_CLIENT = new monitoring.MetricServiceClient();
+// let signInTotal = METRICS_CLIENT.createMetricDescriptor({
+//   metricDescriptor: {
+//     description: "The number of sign in requests.",
+//     displayName: "# of sign in",
+//     type: "custom.googleapis.com/user_service/sign_in_total",
+//     metricKind: 'CUMULATIVE',
+//     valueType: 'INT64',
+//   }
+// });
+
+let counter = new promClient.Counter({
+  name: "custom.googleapis.com/user_service/sign_in_total",
+  help: "Total number of sign in requests",
+});
 
 export class SignInHandler extends SignInHandlerInterface {
   public static create(): SignInHandler {
@@ -20,6 +37,7 @@ export class SignInHandler extends SignInHandlerInterface {
     body: SignInRequestBody,
   ): Promise<SignInResponse> {
     console.log(`${loggingPrefix} Start handling SignIn request.`);
+    counter.inc();
     let signedSession = this.sessionBuilder.build(
       JSON.stringify({
         sessionId: `${body.username}-${body.password}`,
