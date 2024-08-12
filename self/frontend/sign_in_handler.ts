0d9560e781c281e952eb3b43ecaa39da5081655b
Yuxuan Xie <teststaybaka@gmail.com>
2024-08-11 21:25:09 -0700

    Revert to test "hello world".

--- self/frontend/sign_in_handler.ts
+++ self/frontend/sign_in_handler.ts
@@ -1,6 +1,3 @@
-import AWS = require("aws-sdk");
-import getStream = require("get-stream");
-import mysql = require("mysql2/promise");
 import {
   SignInRequestBody,
   SignInResponse,
@@ -11,16 +8,10 @@ import { SessionBuilder } from "@selfage/service_handler/session_signer";
 
 export class SignInHandler extends SignInHandlerInterface {
   public static create(): SignInHandler {
-    return new SignInHandler(
-      SessionBuilder.create(),
-      new AWS.S3({ apiVersion: "2006-03-01", region: "us-east-1" }),
-    );
+    return new SignInHandler(SessionBuilder.create());
   }
 
-  public constructor(
-    private sessionBuilder: SessionBuilder,
-    private s3Service: AWS.S3,
-  ) {
+  public constructor(private sessionBuilder: SessionBuilder) {
     super();
   }
 
@@ -29,29 +20,9 @@ export class SignInHandler extends SignInHandlerInterface {
     body: SignInRequestBody,
   ): Promise<SignInResponse> {
     console.log(`${loggingPrefix} Start handling SignIn request.`);
-    let dbPassword = await getStream(
-      this.s3Service
-        .getObject({
-          Bucket: "user-service-secrets",
-          Key: "db_password",
-        })
-        .createReadStream(),
-    );
-    const conn = await mysql.createConnection({
-      host: "user-service-db-cluster.cluster-cda5j8gofxqp.us-east-1.rds.amazonaws.com",
-      port: 3306,
-      database: "UserServiceDb",
-      user: "root",
-      password: dbPassword,
-    });
-    await conn.execute(`CREATE TABLE IF NOT EXISTS User (
-      username varchar(255)
-    );`);
-    conn.destroy();
-
     let signedSession = this.sessionBuilder.build(
       JSON.stringify({
-        sessionId: `${body.username}-${body.password}-${dbPassword}`,
+        sessionId: `${body.username}-${body.password}`,
       } as ClientSession),
     );
     return {
