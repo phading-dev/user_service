822a18178f54ed9599d03c004a60a5b4716c428f
Yuxuan Xie <ykuyo.dev@gmail.com>
2025-03-01 12:50:36 -0800

    Added all_tests.ts

---
+++ local/all_tests.ts
@@ -0,0 +1,17 @@
+import "../node/get_account_contact_handler_test";
+import "../node/get_account_summary_handler_test";
+import "../node/list_account_capabilities_updating_tasks_handler_test";
+import "../node/process_account_capabilities_updating_task_handler_test";
+import "../node/sync_billing_account_state_handler_test";
+import "../web/self/create_account_handler_test";
+import "../web/self/get_account_and_user_handler_test";
+import "../web/self/get_video_player_settings_handler_test";
+import "../web/self/list_accounts_handler_test";
+import "../web/self/save_video_player_settings_handler_test";
+import "../web/self/sign_in_handler_test";
+import "../web/self/sign_up_handler_test";
+import "../web/self/switch_account_handler_test";
+import "../web/self/update_account_handler_test";
+import "../web/self/update_password_handler_test";
+import "../web/self/update_recovery_email_handler_test";
+import "../web/self/upload_account_avatar_handler_test";
