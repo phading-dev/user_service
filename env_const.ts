405beac26d5f590ecefd82a6b6eccea9bb4a9e98
Ykuyo <ykuyo.dev@gmail.com>
2025-06-12 14:30:18 -0700

    Renamed key file name.

--- env_const.ts
+++ env_const.ts
@@ -1,7 +1,7 @@
 import { ENV_VARS } from "./env_vars";
 
 ENV_VARS.spannerDatabaseId = "user-db";
-ENV_VARS.passwordSignerSecretFile = "password_signer_secret";
+ENV_VARS.passwordSignerSecretFile = "password_signer_secret_key";
 ENV_VARS.releaseServiceName = "user-service";
 ENV_VARS.port = 8080;
 ENV_VARS.builderAccount = "user-service-builder";
