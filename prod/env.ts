4f8773b73be0b927edbaa9939f7f1cbf55bdf66b
Ykuyo <ykuyo.dev@gmail.com>
2025-07-26 12:39:44 -0700

    Renamed public access domain.

--- prod/env.ts
+++ prod/env.ts
@@ -4,7 +4,7 @@ import { ENV_VARS } from "../env_vars";
 
 ENV_VARS.spannerInstanceId = ENV_VARS.balancedSpannerInstanceId;
 ENV_VARS.r2AvatarBucketName = "avatar";
-ENV_VARS.r2AvatarPublicAccessOrigin = "https://avatar-prod.secount.com";
+ENV_VARS.r2AvatarPublicAccessOrigin = "https://avatar.secount.com";
 ENV_VARS.emailVerificationEmailTemplateId = "d-6f063cbe264b4dfd877d6ecf3f5843cc";
 ENV_VARS.passwordResetEmailTemplateId = "d-e5fe5fbe7e944f8981497009ba1cb8bc";
 ENV_VARS.replicas = 2;
